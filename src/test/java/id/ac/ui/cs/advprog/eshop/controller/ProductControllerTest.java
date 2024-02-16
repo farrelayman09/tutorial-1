bbfc90fc8fa1e7a352ef8fbc2ee301054c15a0b7
farrelayman09 <farrel.ayman09@gmail.com>
2024-02-16 09:39:42 +0700

    added unit test for ProductController ie. createProductPage, productListPage, & createProductPost

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/controller/ProductControllerTest.java
@@ -0,0 +1,59 @@
+package id.ac.ui.cs.advprog.eshop.controller;
+
+import id.ac.ui.cs.advprog.eshop.model.Product;
+import id.ac.ui.cs.advprog.eshop.service.ProductService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+class ProductControllerTest {
+
+    @Mock
+    private ProductService productService;
+
+    @Mock
+    private Model model;
+
+    @InjectMocks
+    private ProductController productController;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    void testCreateProductPage() {
+        String viewName = productController.createProductPage(model);
+        assertEquals("createProduct", viewName);
+        verify(model).addAttribute(eq("product"), any(Product.class));
+    }
+
+    @Test
+    void testCreateProductPost() {
+        Product product = new Product();
+        String viewName = productController.createProductPost(product, model);
+        assertEquals("redirect:list", viewName);
+        verify(productService).create(product);
+    }
+
+    @Test
+    void testProductListPage() {
+        List<Product> productList = new ArrayList<>();
+        when(productService.findAll()).thenReturn(productList);
+
+        String viewName = productController.productListPage(model);
+
+        assertEquals("productList", viewName);
+        verify(model).addAttribute("products", productList);
+    }
+}
