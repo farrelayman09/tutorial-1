d88794d1896a36caeb6361ec3899c3c1d660234c
farrelayman09 <farrel.ayman09@gmail.com>
2024-03-06 16:56:25 +0700

    [RED] Add tests for Payment model

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
@@ -0,0 +1,106 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import id.ac.ui.cs.advprog.eshop.enums.OrderStatus;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PaymentTest {
+    private List<Product> products;
+    private Map<String, String> paymentData;
+
+    @BeforeEach
+    void setUp() {
+        this.paymentData = new HashMap<>();
+        this.products = new ArrayList<>();
+
+        Product product1 = new Product();
+        product1.setProductId("eb558e9f-1c39-460e-8860-71af6af63bd6");
+        product1.setProductName("Sampo Cap Bambang");
+        product1.setProductQuantity(2);
+
+        Product product2 = new Product();
+        product2.setProductId("a2c62328-4137-4664-83c7-f32db8620155");
+        product2.setProductName("Sabun Cap Usep");
+        product2.setProductQuantity(1);
+        this.products.add(product1);
+        this.products.add(product2);
+
+        order = new Order(
+                "dbd4aff4-9a7f-4603-92c2-eaf529271cc9",
+                products,
+                1708560000L,
+                "Safira Sudrajat"
+        );
+    }
+
+    void putTransferPaymentData() {
+        paymentData.put("bankName", "Mandiri");
+        paymentData.put("referenceCode", "6248103975");
+    }
+
+
+    @Test
+    void testCreateOrderDefaultStatus() {
+        Payment payment = new Payment("a2a5b551-112b-4c0f-d546-84ea1396c79e",
+                PaymentMethod.BANK.getValue(), order, paymentData);
+
+        assertEquals("13652556-012a-4c07-b546-54eb1396d79b", payment.getId());
+        assertEquals(PaymentMethod.BANK.getValue(), payment.getMethod());
+        assertEquals(order, payment.getOrder());
+        assertEquals(paymentData, payment.getPaymentData());
+        assertEquals(PaymentStatus.PROCESSING.getValue(), payment.getStatus());
+    }
+
+    @Test
+    void testCreatePaymentSuccessStatus() {
+        Payment payment = new Payment("a2a5b551-112b-4c0f-d546-84ea1396c79e",
+                PaymentMethod.BANK.getValue(), order, paymentData, PaymentStatus.SUCCESS.getValue());
+        assertEquals(PaymentStatus.SUCCESS.getValue(), payment.getStatus());
+    }
+
+    @Test
+    void testCreatePaymentInvalidStatus() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Payment payment = new Payment("a2a5b551-112b-4c0f-d546-84ea1396c79e",
+                    PaymentMethod.BANK.getValue(), order, paymentData, "MEOW");
+        });
+    }
+
+    @Test
+    void testCreatePaymentRejectedStatus() {
+        Payment payment = new Payment("a2a5b551-112b-4c0f-d546-84ea1396c79e",
+                PaymentMethod.BANK.getValue(), order, paymentData, PaymentStatus.REJECTED.getValue());
+        assertEquals(PaymentStatus.REJECTED.getValue(), payment.getStatus());
+    }
+
+    @Test
+    void testSetStatusToRejected() {
+        Payment payment = new Payment("a2a5b551-112b-4c0f-d546-84ea1396c79e",
+                PaymentMethod.BANK.getValue(), order, paymentData, PaymentStatus.SUCCESS.getValue());
+        payment.setStatus(OrderStatus.CANCELLED.getValue());
+        assertEquals(OrderStatus.CANCELLED.getValue(), payment.getStatus());
+    }
+
+    @Test
+    void testSetStatusToInvalidStatus() {
+        Payment payment = new Payment("a2a5b551-112b-4c0f-d546-84ea1396c79e",
+                PaymentMethod.BANK.getValue(), order, paymentData);
+        assertThrows(IllegalArgumentException.class, () -> order.setStatus("MEOW"));
+    }
+
+    @Test
+    void testCreatePaymentWithNullOrder() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            @SuppressWarnings("unused")
+            Payment payment = new Payment("a2a5b551-112b-4c0f-d546-84ea1396c79e",
+                    PaymentMethod.BANK.getValue(), null, paymentData);
+        });
+    }
+}
