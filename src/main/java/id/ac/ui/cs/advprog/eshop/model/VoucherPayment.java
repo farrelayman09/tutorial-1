50775ddab0e430e93f0e3ce72a96a9623c6c707f
farrelayman09 <farrel.ayman09@gmail.com>
2024-03-06 19:12:10 +0700

    [GREEN] Implement Voucher Payment model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/VoucherPayment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/VoucherPayment.java
@@ -5,9 +5,44 @@ import java.util.Map;
 public class VoucherPayment extends Payment{
     public VoucherPayment(String id, String method, Order order, Map<String, String> paymentData) {
         super(id, method, order, paymentData);
+        setPaymentData(paymentData);
     }
 
     public VoucherPayment(String id, String method, Order order, Map<String, String> paymentData, String status) {
         super(id, method, order, paymentData, status);
+        setPaymentData(paymentData);
+    }
+
+    @Override
+    public void setPaymentData(Map<String, String> paymentData) {
+        if (paymentData == null || paymentData.isEmpty()) {
+            throw new IllegalArgumentException("Empty Payment!");
+        }
+        else if (paymentData.get("voucherCode") == null || paymentData.get("voucherCode").isEmpty()) {
+            throw new IllegalArgumentException("Empty Voucher Code!");
+        }
+        else if (!isVoucherCodeValid(paymentData.get("voucherCode"))) {
+            throw new IllegalArgumentException("Invalid Voucher Code!");
+        }
+
+        this.paymentData = paymentData;
+    }
+
+
+    private long numericsCount(String voucherCode) {
+        long count = 0;
+        for (int i = 0; i < voucherCode.length(); i++) {
+            char c = voucherCode.charAt(i);
+            if (Character.isDigit(c)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+
+    private boolean isVoucherCodeValid(String voucherCode) {
+        return voucherCode.length() == 16 && voucherCode.startsWith("ESHOP") &&
+                numericsCount(voucherCode) == 8;
     }
 }
